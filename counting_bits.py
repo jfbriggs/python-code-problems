fecc291744f3160ad0c591b705f28a299a096834
James Briggs <jfbriggs106@gmail.com>
2019-09-02 09:13:12 -0700

    Add instructions, base function code for counting_bits

---
+++ counting_bits.py
@@ -0,0 +1,14 @@
+"""
+Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them as an array.
+
+Example:
+Input: 2
+Output: [0, 1, 1]
+"""
+
+def countBits(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        
