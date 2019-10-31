9cb1ea73309457ae8cc988ad88a698bc9e5f502f
James Briggs <jfbriggs106@gmail.com>
2019-10-31 11:23:25 -0700

    Create working solution with helper function

--- range_extraction.py
+++ range_extraction.py
@@ -13,7 +13,36 @@ solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20
 """
 
 def solution(args):
-    pass
+    result = ""
+    curr_subrange_start = args[0]
+    curr_subrange_end = args[0]
+
+    def create_substring(start, end, is_last):
+        output = ""
+
+        if curr_subrange_end >= curr_subrange_start + 2: # if the current subrange contains at least 3 values
+            output += str(start) + "-" + str(end)
+        else: # if the current subrange is only 1 or 2 values
+            if curr_subrange_start != curr_subrange_end:
+                output += str(curr_subrange_start) + ","
+            output += str(curr_subrange_end)
+
+        if not is_last:
+            output += ","
+
+        return output
+
+    for i in range(1, len(args)):
+        if args[i] > curr_subrange_end + 1: # if the current value is more than 1 greater than the prior (meaning the current subrange is ending)
+            result += create_substring(curr_subrange_start, curr_subrange_end, False)
+            curr_subrange_start = args[i]
+            curr_subrange_end = args[i]
+        else: # if the current value is just 1 greater than the prior value
+            curr_subrange_end += 1
+
+    result += create_substring(curr_subrange_start, curr_subrange_end, True)
+
+    return result
 
 
 
