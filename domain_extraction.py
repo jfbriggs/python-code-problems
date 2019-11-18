331940662cf9eee31aa26cc328f78483854cd51c
James Briggs <jfbriggs106@gmail.com>
2019-11-18 08:51:27 -0800

    Init human_readable_time.py with test code

--- domain_extraction.py
+++ domain_extraction.py
@@ -9,11 +9,24 @@ domain_name("https://www.cnet.com") == "cnet"
 """
 
 def domain_name(url):
-    pass
+    url_split = url.split("/")
+
+    if url_split[0][:4] == "http":
+        domain_iso = url_split[2]
+    else:
+        domain_iso = url_split[0]
+
+    domain_split =  domain_iso.split(".")
+
+    if domain_split[0] == "www":
+        return domain_split[1]
+    else:
+        return domain_split[0]
 
 
 ## TEST CODE ##
 
-domain_name("http://github.com/carbonfive/raygun") # "github"
-domain_name("http://www.zombie-bites.com") # "zombie-bites"
-domain_name("https://www.cnet.com") # "cnet"
+print(domain_name("http://github.com/carbonfive/raygun")) # "github"
+print(domain_name("http://www.zombie-bites.com")) # "zombie-bites"
+print(domain_name("https://www.cnet.com")) # "cnet"
+print(domain_name("www.xakep.ru")) # "xakep"
