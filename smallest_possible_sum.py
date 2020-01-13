29794b72212ab16cd92a3846b7722385a5fd7427
James Briggs <jfbriggs106@gmail.com>
2020-01-13 09:47:09 -0800

    Init smallest_possible_sum

---
+++ smallest_possible_sum.py
@@ -0,0 +1,18 @@
+"""
+Given an array X of positive integers, its elements are to be transformed by running the following operation on them as many times as required:
+
+if X[i] > X[j] then X[i] = X[i] - X[j]
+
+When no more transformations are possible, return its sum ("smallest possible sum").
+
+Example
+solution([6, 9, 21]) #-> 9
+
+Solution steps:
+[6, 9, 12] #-> X[2] = 21 - 9
+[6, 9, 6] #-> X[2] = 12 - 6
+[6, 3, 6] #-> X[1] = 9 - 6
+[6, 3, 3] #-> X[2] = 6 - 3
+[3, 3, 3] #-> X[1] = 6 - 3
+
+"""
