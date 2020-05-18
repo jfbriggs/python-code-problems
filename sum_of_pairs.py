956f9ff9f1dd77ed1165133aa4a5a9723ed188f0
James Briggs <jfbriggs106@gmail.com>
2020-05-18 10:16:05 -0700

    Adjust test code

--- sum_of_pairs.py
+++ sum_of_pairs.py
@@ -38,11 +38,11 @@ l6= [4, -2, 3, 3, 4]
 l7= [0, 2, 0]
 l8= [5, 9, 13, -3]
 
-print(sum_pairs(l1, 8) # "Basic: %s should return [1, 7] for sum = 8" % l1)
-print(sum_pairs(l2, -6) # "Negatives: %s should return [0, -6] for sum = -6" % l2)
-print(sum_pairs(l3, -7) # "No Match: %s should return None for sum = -7" % l3)
-print(sum_pairs(l4, 2) # "First Match From Left: %s should return [1, 1] for sum = 2 " % l4)
-print(sum_pairs(l5, 10) # "First Match From Left REDUX!: %s should return [3, 7] for sum = 10 " % l5)
-print(sum_pairs(l6, 8) # "Duplicates: %s should return [4, 4] for sum = 8" % l6)
-print(sum_pairs(l7, 0) # "Zeroes: %s should return [0, 0] for sum = 0" % l7)
-print(sum_pairs(l8, 10) # "Subtraction: %s should return [13, -3] for sum = 10" % l8)
+print(sum_pairs(l1, 8)) # Basic: [1, 7]
+print(sum_pairs(l2, -6)) # Negatives: [0, -6]
+print(sum_pairs(l3, -7)) # No Match: None
+print(sum_pairs(l4, 2)) # First Match From Left: [1, 1]
+print(sum_pairs(l5, 10)) # First Match From Left REDUX!: [3, 7]
+print(sum_pairs(l6, 8)) #"Duplicates: [4, 4]
+print(sum_pairs(l7, 0)) # Zeroes:[0, 0]
+print(sum_pairs(l8, 10)) # Subtraction: [13, -3]
