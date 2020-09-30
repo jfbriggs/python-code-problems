aebf5ccba1790caec77a585a54013b43a3695294
James Briggs <jfbriggs106@gmail.com>
2020-09-30 10:28:42 -0700

    Init max_square.py

---
+++ max_square.py
@@ -0,0 +1,30 @@
+"""
+Given a 2D binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+
+Example:
+
+Input:
+
+1 0 1 0 0
+1 0 1 1 1
+1 1 1 1 1
+1 0 0 1 0
+
+Output: 4
+
+"""
+
+def maximal_square(matrix):
+    pass
+
+
+## TEST CODE ##
+
+m = [
+    [1, 0, 1, 0, 0],
+    [1, 0, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 0, 0, 1, 0]
+]
+
+print(maximal_square(m)) # 4
